623e6d56b7c96f54859f4177d970ced4d873e0b5
Cari van der Merwe <cvandermerwe@lcs.on.ca>
2024-01-18 14:49:35 -0500

    Initial Commit

---
+++ TradingCards/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TradingCards
+//
+//  Created by Cari van der Merwe on 2024-01-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TradingCards/TradingCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradingCardsApp.swift
+//  TradingCards
+//
+//  Created by Cari van der Merwe on 2024-01-18.
+//
+
+import SwiftUI
+
+@main
+struct TradingCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
