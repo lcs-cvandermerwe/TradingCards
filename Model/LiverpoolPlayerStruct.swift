3071472997e3dab40941410573cfcb72960aeb63
Cari van der Merwe <cvandermerwe@lcs.on.ca>
2024-01-29 09:38:25 -0500

    Added extra details into struct for layout of card.

--- Model/LiverpoolPlayerStruct.swift
+++ Model/LiverpoolPlayerStruct.swift
@@ -16,15 +16,15 @@ struct LiverpoolPlayer {
     
 }
 
-let alexanderArnold = LiverpoolPlayer(player: "Name:Alexander-Arnold", age: "Age:25", number: "Number:66", nationality: "Nationality:British", position: "Position:RB")
+let alexanderArnold = LiverpoolPlayer(player: "Alexander-Arnold", age: "Age: 25", number: "Number: 66", nationality: "Nationality: British", position: "Position: RB")
 
-let vanDijk = LiverpoolPlayer(player: "Name:van Dijk", age: "Age:32", number: "Number:4", nationality: "Nationality:Dutch", position: "Position:CB")
+let vanDijk = LiverpoolPlayer(player: "van Dijk", age: "Age: 32", number: "Number: 4", nationality: "Nationality: Dutch", position: "Position: CB")
 
-let robertson = LiverpoolPlayer(player: "Name:Robertson", age: "Age:29", number: "Number:26", nationality: "Nationality:Scottish", position: "Position:LB")
+let robertson = LiverpoolPlayer(player: "Robertson", age: "Age: 29", number: "Number: 26", nationality: "Nationality: Scottish", position: "Position: LB")
 
-let salah = LiverpoolPlayer(player: "Name:Salah", age: "Age:31", number: "Number:11", nationality: "Nationality:Egyptian", position: "Position:F")
+let salah = LiverpoolPlayer(player: "Salah", age: "Age: 31", number: "Number: 11", nationality: "Nationality: Egyptian", position: "Position: F")
 
-let núñez = LiverpoolPlayer(player: "Name:Núñez", age: "Age:24", number: "Number:9", nationality: "Nationality:Uruguayan", position: "Position:F")
+let núñez = LiverpoolPlayer(player: "Núñez", age: "Age: 24", number: "Number: 9", nationality: "Nationality: Uruguayan", position: "Position: F")
 
 let allPlayers = [
 alexanderArnold,
